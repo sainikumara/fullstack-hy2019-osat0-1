69f06d7c6ba5d423247446eaf3f4f307bfb47012
Saini Kumara <saini@iki.fi>
2019-01-28 20:30:03 +0200

    Osa 1: tehtävät 1.3-1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,25 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const course = "Half Stack -sovelluskehitys";
-  const part1 = "Reactin perusteet";
-  const exercises1 = 10;
-  const part2 = "Tiedonvälitys propseilla";
-  const exercises2 = 7;
-  const part3 = "Komponenttien tila";
-  const exercises3 = 14;
+  const course = {
+    name: "Half Stack -sovelluskehitys",
+    parts: [
+    {
+      name: "Reactin perusteet",
+      exercises: 10
+    },
+    {
+      name: "Tiedonvälitys propseilla",
+      exercises: 7
+    },
+    {
+      name: "Komponenttien tila",
+      exercises: 14
+    }
+  ]
+}
 
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3} />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   );
 };
@@ -36,9 +40,9 @@ const Header = props => {
 const Content = props => {
   return (
     <div>
-      <Part part={props.part1} exercises={props.exercises1} />
-      <Part part={props.part2} exercises={props.exercises2} />
-      <Part part={props.part3} exercises={props.exercises3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div>
   );
 };
@@ -47,17 +51,18 @@ const Part = props => {
   return (
     <div>
       <p>
-        {props.part} {props.exercises}
+        {props.part.name} {props.part.exercises}
       </p>
     </div>
   );
 };
 
 const Total = props => {
+  console.log(props);
   return (
     <div>
       <p>
-        yhteensä {props.exercises1 + props.exercises2 + props.exercises3}{" "}
+        yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises }
         tehtävää
       </p>
     </div>
